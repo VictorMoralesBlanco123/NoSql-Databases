7dff38f60585cd947d4d7521cf47be783f1ce1b9
LWTC <LWIT@prod.ccps.loc>
2022-03-30 08:35:08 -0400

    update

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb');
+var mongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/gym';
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
