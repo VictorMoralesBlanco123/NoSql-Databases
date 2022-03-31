a2f32e5cd887b6797be688f7203c30d505370806
LWTC <LWIT@prod.ccps.loc>
2022-03-31 08:42:24 -0400

    update

---
+++ Frontend/src/components/RegisterClientForm.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="fixMargins">
+    <div class="row">
+      <div class="col">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="First Name"
+          required
+        />
+      </div>
+      <div class="col">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Last Name"
+          required
+        />
+      </div>
+    </div>
+    <br />
+    <div class="row">
+      <div class="col">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Address"
+          required
+        />
+      </div>
+    </div>
+    <br />
+    <div class="row">
+      <div class="col">
+        <input
+          type="tel"
+          class="form-control"
+          placeholder="Phone Number"
+          minlength="12"
+          maxlength="12"
+          ref="phone"
+          pattern="[0-9\-]"
+          @change="check"
+          required
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+let phone = ref(null);
+
+function check(e) {
+  if (
+    event.key != "Backspace" &&
+    (phone.value.length === 3 || phone.value.length === 7)
+  ) {
+    phone.value += "-";
+  }
+}
+</script>
+
+<style scoped>
+.fixMargins {
+  width: 50% !important;
+  margin-right: auto;
+  margin-left: auto;
+}
+</style>
--- Frontend/src/views/AddClientView.vue
+++ Frontend/src/views/AddClientView.vue
@@ -1,3 +1,19 @@
 <template>
-    
+  <div class="container-fluid center">
+    <div class="row">
+      <div class="col">
+        <br>
+        <h1>Register Client</h1>
+        <br>
+        <Register />
+      </div>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import Register from "@/components/RegisterClientForm.vue";
+</script>
+
+<style scoped>
+</style>
