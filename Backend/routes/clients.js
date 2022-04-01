b36b28645a12783a75d6648eb741cdb18ca70485
LWTC <LWIT@prod.ccps.loc>
2022-04-01 09:54:49 -0400

    table done

--- Backend/routes/clients.js
+++ Backend/routes/clients.js
@@ -6,7 +6,6 @@ var url = 'mongodb://localhost:27017/gym';
 
 router.post('/add', function (req, res) {
     let form = req.body;
-    console.log(form)
     mongoClient.connect(url, { useUnifiedTopology: true }, (err, db) => {
         if (err) throw err;
         let dbo = db.db('gym');
@@ -18,17 +17,17 @@ router.post('/add', function (req, res) {
     });
 });
 
-router.get('/list', function(req,res){
-    mongoClient.connect(url, {useUnifiedTopology: true}, (err, db) => {
+router.get('/list', function (req, res) {
+    mongoClient.connect(url, { useUnifiedTopology: true }, (err, db) => {
         if (err) throw err;
         let dbo = db.db('gym');
         dbo.collection('clients').find().toArray((err, result) => {
-          if (err) throw err;
-          db.close();
-          res.send(JSON.stringify(result));
-          res.end();
+            if (err) throw err;
+            db.close();
+            res.send(JSON.stringify(result));
+            res.end();
         });
-      });
+    });
 })
 
 module.exports = router;
--- Frontend/src/components/ClientCards.vue
+++ Frontend/src/components/ClientCards.vue
@@ -1,32 +1,49 @@
 <template>
-  <div class="col">
-    <div class="row space-2" id="booksCol">
-      <div v-for="(client, i) in clients" :key="i" class="col-4">
-        <div class="card text-dark" style="width: 18rem; margin: 2%">
-          <div class="card-body">
-            <p class="card-title">
-              Name: {{ client.first_name + " " + client.last_name }}
-            </p>
-            <p class="card-text">Address: {{ client.address }}</p>
-            <p class="card-text">Phone Number: {{ client.phone }}</p>
-
-            <button
-              class="btn btn-success"
-              data-toggle="modal"
-              v-on:click="Edit(client._id)"
-              data-target="#BookModal"
-            >
-              Edit
-            </button>
-            <button
-              class="btn btn-warning"
-              v-on:click="deleteBook(book.book_id)"
-            >
-              Delete
-            </button>
-          </div>
-        </div>
-      </div>
+  <div class="container-fluid fix">
+    <form class="d-flex" @submit="search">
+      <input
+        class="form-control me-2"
+        type="search"
+        placeholder="Search"
+        aria-label="Search"
+        v-model="search"
+      />
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+    <br />
+    <div class="row">
+      <table class="table">
+        <thead class="table-dark">
+          <tr>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Address</th>
+            <th scope="col">Phone</th>
+            <th scope="col" style="width: 200px">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(client, i) in clients" :key="i">
+            <td>{{ client.first_name }}</td>
+            <td>{{ client.last_name }}</td>
+            <td>{{ client.address }}</td>
+            <td>{{ client.phone }}</td>
+            <td>
+              <button
+                class="btn btn-success me-2"
+                data-toggle="modal"
+                v-on:click="Edit(client._id)"
+                data-target="#BookModal"
+              >
+                Edit
+              </button>
+              <button class="btn btn-warning" v-on:click="Delete(client._id)">
+                Delete
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -35,16 +52,21 @@
 import { ref, onMounted } from "vue";
 
 let clients = ref(null);
+let search = ref(null);
 
-onMounted(() => {
-  fillArray();
-});
-
-async function fillArray() {
+onMounted(async () => {
   let res = await fetch("http://localhost:4000/clients/list");
   clients.value = await res.json();
+});
+
+function search(){
+    
 }
 </script>
 
 <style scoped>
+.fix {
+  margin-top: 2%;
+  width: 55%;
+}
 </style>
